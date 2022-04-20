e62e79d5f71bf704af8b03aef5f1a7c2dc96914a
alb <alb@pop-os.localdomain>
2022-04-20 13:38:45 +0500

    somesing

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'rpoi6u#1bherrxl2hb&=bj0v(q83h9tv8qd$%*=2rwm6fiwh9m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
